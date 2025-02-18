39c07f83dbc0046961adeac5f1e2de45a2c787ce
ddt009 <ddt009@users.noreply.github.com>
2025-02-18 11:03:10 +0800

    Update m-git.sh
    fix:line 83 'then' error

--- m-git.sh
+++ m-git.sh
@@ -21,7 +21,6 @@ AddGitGroup(){
 }
 
 InitGitRepo(){
-    
     AddGitGroup
     rsp_path=${1}
     # 路径前加上DEFAULT_REPO_ROOT生成绝对路径
@@ -34,7 +33,6 @@ InitGitRepo(){
     if [[ ${rsp_path} != *".git" ]]; then
         rsp_path="${rsp_path}.git"
     fi
-:
     if [ ! -d "${rsp_path}/.git" ]; then
         echo "Init .git in ${rsp_path}"
         mkdir -p ${rsp_path}
@@ -54,12 +52,11 @@ ChangGitUserSShKey(){
         username=${GIT_USER_FIX}${1}
     fi
 
-    if [  -d "/home/${username}" ]; then
+    if [ -d "/home/${username}" ]; then
         workdir=/home/${username}/.ssh
-        
         mkdir -p ${workdir}
 
-        if [  -f "${workdir}/authorized_keys" ]; then
+        if [ -f "${workdir}/authorized_keys" ]; then
             current_time=$(date +"%Y%m%d%H%M")
             mkdir -p ${workdir}/${current_time}
             mv ${workdir}/id_rsa* ${workdir}/${current_time}/
@@ -67,7 +64,7 @@ ChangGitUserSShKey(){
             echo "Move origin key file to ${current_time}"
         fi
         echo "Generating new id_rsa"
-        ssh-keygen -t rsa -b 4096 -C "${username}@gt-users" -f ${workdir}/id_rsa
+        ssh-keygen -t rsa -b 4096 -C "${username}@gt-users" -f ${workdir}/id_rsa -q -N ""
         mv ${workdir}/id_rsa.pub ${workdir}/authorized_keys
         chown -R ${username}:${username} ${workdir}
         chmod 600 ${workdir}/authorized_keys
@@ -78,7 +75,7 @@ ChangGitUserSShKey(){
 }
 
 # 确保用户限定访问目录及其以上目录为root:root 755
-function fix_chroot_permissions() {
+fix_chroot_permissions(){
     current_dir=${DEFAULT_CHROOT_BASE}
     while [[ $current_dir != / ]]; do
         if [[ $(stat -c '%U:%G' $current_dir) != 'root:root' ]]; then
@@ -96,7 +93,7 @@ function fix_chroot_permissions() {
 
 # 用户限定目录前的准备工作
 SetChrootEnv(){
-    if [ ! -f "${DEFAULT_CHROOT_BASE}/usr/bin/git-shell" ];then
+    if [ ! -f "${DEFAULT_CHROOT_BASE}/usr/bin/git-shell" ]; then
         mkdir -p ${DEFAULT_CHROOT_BASE}/{usr/bin,bin,lib,lib64}
 
         fix_chroot_permissions
@@ -106,35 +103,32 @@ SetChrootEnv(){
         cp /usr/bin/git-shell ${DEFAULT_CHROOT_BASE}/usr/bin/
         ldd /usr/bin/git-shell | grep -o '/[^ ]*' | xargs -I '{}' cp '{}' ${DEFAULT_CHROOT_BASE}/lib/
         ldd /usr/bin/git-shell | grep -o '/lib64/[^ ]*' | xargs -I '{}' cp '{}' ${DEFAULT_CHROOT_BASE}/lib64/
+        
         # 挂载系统目录
         mkdir -p ${DEFAULT_CHROOT_BASE}/{dev,proc,sys}
         mount --bind /dev ${DEFAULT_CHROOT_BASE}/dev
-        #mount --bind /proc ${DEFAULT_CHROOT_BASE}/proc
-        #mount --bind /sys ${DEFAULT_CHROOT_BASE}/sys
-        # 挂载登记进fstab内(重启有效)
-        CHECK_FATAB_DATA=$(cat /etc/fstab|grep "${DEFAULT_CHROOT_BASE}/dev")
-        if [[ -z $CHECK_FATAB_DATA ]]; then
-            echo "echo \"/dev  ${DEFAULT_CHROOT_BASE}/dev none   bind    0   0\" >> /etc/fstab"
+        
+        # 挂载登记进fstab
+        CHECK_FSTAB_DATA=$(grep "${DEFAULT_CHROOT_BASE}/dev" /etc/fstab)
+        if [[ -z $CHECK_FSTAB_DATA ]]; then
             echo "/dev  ${DEFAULT_CHROOT_BASE}/dev none   bind    0   0" >> /etc/fstab
         fi
-    fi            
+    fi
 }
 
 # 限定git-users用户组目录
 ChangeSshConfig(){
-
     SetChrootEnv
 
     sshd_config=/etc/ssh/sshd_config
     if grep -q "^Match Group ${GIT_GROUP_NAME}" "${sshd_config}"; then
         echo "Group ${GIT_GROUP_NAME} already been matched in ssh_config"
     else
-        echo "Add group match in ssh_config"
-        (cat <<HERE
+        echo "Adding group match in ssh_config"
+        cat <<EOF >>${sshd_config}
 Match Group ${GIT_GROUP_NAME}
     ChrootDirectory ${DEFAULT_CHROOT_BASE}
-HERE
-)>>${sshd_config}
+EOF
     fi
     systemctl restart sshd
 }
@@ -146,27 +140,22 @@ AddGitUser(){
     fi
 
     if ! id "$username" &>/dev/null; then
-        
         AddGitGroup
-
-        echo "Add user ${username}"
+        echo "Adding user ${username}"
         useradd --shell /usr/bin/git-shell --groups ${GIT_GROUP_NAME} ${username}
 
         mkdir -p /home/${username}/.ssh
-
         chown -R ${username}:${username} /home/${username}
 
         ChangGitUserSShKey ${username}
-
-        ChangeSshConfig 
-
+        ChangeSshConfig
     else
-        echo "${username} exists."
+        echo "User ${username} already exists."
     fi
 }
 
+# 参数解析
 if [ "$#" -lt 1 ]; then
-
     echo "Usage: $0 <git-user>              create git user"
     echo "Usage: $0 <git-user> [git-repo]   create git user and repository"
     echo "Usage: $0 -p   <git-repo>         create git repository"
@@ -174,41 +163,37 @@ if [ "$#" -lt 1 ]; then
     exit 1
 fi
 
-if [ "$1" = "-p" ] ; then 
-    if [ "$2" == "" ] ;then
+if [ "$1" = "-p" ]; then
+    if [ -z "$2" ]; then
         echo "git-repo required"
     else
-        InitGitRepo $2
+        InitGitRepo "$2"
     fi
     exit 1
 fi
 
-if [ "$1" = "-key" ] ; then 
-    if [ "$2" == "" ] ;then
+if [ "$1" = "-key" ]; then
+    if [ -z "$2" ]; then
         echo "git-user required"
     else
-
-        ChangGitUserSShKey $2
-    
+        ChangGitUserSShKey "$2"
     fi
     exit 1
 fi
 
-if  [ "$#" -eq 1 ] || [ "$#" -eq 2 ]; then
-    if [[ ${1} == "-"* ]]; then
-        echo "illegal paramete $1"
+if [ "$#" -eq 1 ] || [ "$#" -eq 2 ]; then
+    if [[ "$1" == "-"* ]]; then
+        echo "Illegal parameter: $1"
         exit 1
     fi
-    
-    AddGitUser $1
 
-    if [ "$2" != "" ] ;then
-        if [[ ${2} == "-"* ]]; then
-            echo "illegal paramete 2"
+    AddGitUser "$1"
+
+    if [ -n "$2" ]; then
+        if [[ "$2" == "-"* ]]; then
+            echo "Illegal parameter: $2"
             exit 1
         fi
-
-        InitGitRepo $2
-
+        InitGitRepo "$2"
     fi
 fi
